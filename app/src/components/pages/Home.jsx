95797528e6517de8f92bbdde498e4e7e852508ec
Tyler Consalvi <106493956+tkcthulhu@users.noreply.github.com>
2023-05-23 17:21:20 -0400

    Update Home.jsx

--- app/src/components/pages/Home.jsx
+++ app/src/components/pages/Home.jsx
@@ -1,47 +1,84 @@
 import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
-function Home(props) {
+import '../../css/home.css';
+
+import slider_1 from '../../images/Slide_1.jpeg';
+import slider_2 from '../../images/Slide_2.jpeg';
+import slider_3 from '../../images/Slide_3.jpeg';
+
+export function Home(props) {
 
     const [index, setIndex] = useState(0);
 
     const handleSelect = (selectedIndex, e) => {
         setIndex(selectedIndex);
-      };
+    };
 
     return (
         <>
-            <Carousel activeIndex={index} onSelect={handleSelect}>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=First slide&bg=373940"
-                    alt="First slide"
-                    />
+            <Carousel activeIndex={index} onSelect={handleSelect} className="home-carousel">
+                <Carousel.Item className='carousel-item'>
+                    <div className="d-block w-100 carousel-slide" style={{backgroundImage: `url(${slider_1}`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover',}}>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col d-flex justify-content-center">
+                                    <h1 className="text-white">
+                                        TEST
+                                    </h1>
+                                </div>
+                            </div>
+                            <div className="row justify-content-center">
+                                <div className="col">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <Carousel.Caption>
                     <h3>First slide label</h3>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Second slide&bg=282c34"
-                    alt="Second slide"
-                    />
+                <Carousel.Item className='carousel-item'>
+                    <div className="d-block w-100 carousel-slide" style={{backgroundImage: `url(${slider_2}`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover',}}>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col d-flex justify-content-center">
+                                    <h1 className="text-white">
+                                        TEST
+                                    </h1>
+                                </div>
+                            </div>
+                            <div className="row justify-content-center">
+                                <div className="col">
 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <Carousel.Caption>
                     <h3>Second slide label</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Third slide&bg=20232a"
-                    alt="Third slide"
-                    />
+                <Carousel.Item className='carousel-item'>
+                    <div className="d-block w-100 carousel-slide" style={{backgroundImage: `url(${slider_3}`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover',}}>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col d-flex justify-content-center">
+                                    <h1 className="text-white">
+                                        TEST
+                                    </h1>
+                                </div>
+                            </div>
+                            <div className="row justify-content-center">
+                                <div className="col">
 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <Carousel.Caption>
                     <h3>Third slide label</h3>
                     <p>
@@ -53,5 +90,3 @@ function Home(props) {
         </>
     )
 }
-
-export default Home;
