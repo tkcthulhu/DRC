503a09244fa59748dd274030790549cb337d4c60
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2023-05-29 17:57:01 +0000

    consultant content added

--- app/src/components/pages/services/categories/Automation_Consulting.jsx
+++ app/src/components/pages/services/categories/Automation_Consulting.jsx
@@ -1,6 +1,32 @@
 function Automation_Consulting(props) {
     return (
         <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        insert pic here
+                    </div>
+                    <div className="col-md-6">
+                        <h4>
+                            Our team of seasoned consultants guides you through the intricacies of automation technology, offering comprehensive guidance and strategic insights at every stage of your automation journey.
+                            <br/>
+                            At Dead Right Controls, we offer a distinguished Automation Consultant service to guide you through the intricacies of automation technology. Our team of seasoned consultants possesses an exceptional blend of industry expertise, strategic vision, and a deep understanding of emerging trends and advancements in automation.
+                        </h4>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6">
+                        <h4>
+                            As your trusted partner, we are committed to providing comprehensive guidance and strategic insights at every stage of your automation journey. We work closely with your organization to understand your unique challenges, objectives, and operational requirements. Leveraging our extensive experience, we offer tailored recommendations and solutions to optimize your automation strategies and drive your success.
+                            <br/>
+                            At Dead Right Controls, we recognize that automation is not a one-size-fits-all approach. Our consultants utilize their deep knowledge and understanding of diverse industries to customize solutions that address your specific needs. Whether it's enhancing productivity, improving process efficiency, or streamlining workflows, we leverage cutting-edge technologies and best practices to craft a roadmap that aligns with your business goals.
+                        </h4>
+                    </div>
+                    <div className="col-md-6">
+                        insert pic here
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
